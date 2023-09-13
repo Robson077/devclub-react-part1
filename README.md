580d719c9f93c8af37d094ca8be7cc1579ce1269
Robson077 <87337161+Robson077@users.noreply.github.com>
2023-09-12 22:02:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# devclub-react-part1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-jscfd2)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hellitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
