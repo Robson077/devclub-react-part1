d96723c7892fc5a6adb89ef7c6b8b4d37bab2a93
Robson077 <87337161+Robson077@users.noreply.github.com>
2023-09-13 15:38:59 -0300

    feat: error styled

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import styled from "styled-components"
 
 
-const Caixa = styled.div`
-  background: black;
-
-`
-
 const Title = styled.h1`
-  font-size: 50px;
-`
+  color: red;
+`;
 
 
 export default function App() {
