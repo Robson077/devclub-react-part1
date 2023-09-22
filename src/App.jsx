8e5f5734ad03ae6ff43a552d7f707514d9a0c21a
Robson Santana <87337161+Robson077@users.noreply.github.com>
2023-09-22 11:05:39 +0000

    feat: add styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import {Title} from "./styles"
 
 function App() {
   const hello = "heo world"
 
   return (
     <div>
-      <h1>{hello}</h1>
+      <Title>{hello}</Title>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
