f5ea3d021aa2825e5d6ee0820566c3dcaf618dbd
Robson Santana <87337161+Robson077@users.noreply.github.com>
2023-09-22 10:28:02 +0000

    Update 4 files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  const hello = "hello world"
+
+  return (
+    <div>
+      <h1>{hello}</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+
+export default App
